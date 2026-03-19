218dce84a5adf6f17c877301fff396efa2499be5
Diego Ledesma Gallegos <diegolg@lcg.unam.mx>
2026-03-18 20:29:46 -0600

    feature: Agregando efecto a la tabla

--- src/regulon_summary.py
+++ src/regulon_summary.py
@@ -1,5 +1,5 @@
-
 regulon = {}
+clas = {}
 
 interacciones = [
     ("AraC", "araA", "+"),
@@ -23,12 +23,25 @@ else:
         # Agrego el target al diccionario de su tf
         if target not in regulon[tf]:
             regulon[tf].append(target)
+        
+        if efecto == "":
+            continue
+        if tf not in clas:
+            clas[tf] = []
+            if efecto == "+":
+                clas[tf] = ["Activador"]
+            elif efecto == "-":
+                clas[tf] = ["Represor"]
+            else:
+                clas[tf] = ["Dual"]
+            
 
 # imprimir tabla final
-print("Gen | No. de genes que regula | Genes regulados")
+print("Gen | No. de genes que regula | Genes regulados | Efecto")
 
 for tf in regulon:
     numero = len(regulon[tf])
-    genes = ", ".join(regulon[tf]) 
+    genes = ", ".join(regulon[tf])
+    efect = clas[tf]
     
-    print(tf, "|", numero, "|", genes)
+    print(tf, "|", numero, "|", genes, "|", efect)
